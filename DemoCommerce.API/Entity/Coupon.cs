16a0cf4030c80295f7c4081b9bc0ae6259b169f4
Ahmed Ajala <ajalaahmed8@gmail.com>
2024-07-21 10:15:56 +0100

    Add project files.

---
+++ DemoCommerce.API/Entity/Coupon.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoCommerce.API.Entity
+{
+    public class Coupon
+    {
+        public int Id { get; set; }   
+        public int CouponId { get; set; }       
+        public required  string CouponCode { get; set; }
+        public double MinAmount { get; set; }                                                             
+    }
+}
