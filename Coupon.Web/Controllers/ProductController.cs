147465306f843e587195d4a3039eba0520f94416
Ahmed Ajala <ajalaahmed8@gmail.com>
2024-08-02 17:19:28 +0100

    Product view implemented

--- Coupon.Web/Controllers/ProductController.cs
+++ Coupon.Web/Controllers/ProductController.cs
@@ -33,6 +33,8 @@ namespace Commerce.Web.Controllers
             return View(list);
         }
 
+
+        
         public async Task<IActionResult> ProductCreate(ProductDto model)
         {
             if (ModelState.IsValid)
@@ -76,6 +78,39 @@ namespace Commerce.Web.Controllers
             if (response != null && response.IsSuccess)
             {
                 TempData["success"] = "Product deleted successfully";
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(productDto);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductEdit(ProductDto productDto)
+        {
+            ResponseDto? response = await _productService.UpdateProductsAsync(productDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Product updated successfully";
                 return RedirectToAction(nameof(ProductIndex));
             }
             else
